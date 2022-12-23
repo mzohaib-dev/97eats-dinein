d17a47f1ab18f25510067511d0fafb8ea16903d2
shabe <shabeeboali@gmail.com>
2022-12-23 11:42:14 +0530

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-page class="row items-center justify-evenly" style="background:#fdbe0f;">
+    <q-img src="~assets/dine-logo.jpg" width="512px"></q-img>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ src/pages/TableMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page>
+    <div class="row">
+      <div class="col-12 col-md-4"></div>
+      <div class="col-12 col-md-4">
+        <q-card>
+          <q-card-section class="q-pa-none flex justify-center" style="background:#fdbe0f;">
+            <q-img src="~assets/dine-logo.jpg" width="250px"></q-img>
+          </q-card-section>
+          <q-card-section>
+            <q-tabs v-model="tab">
+              <template v-for="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]" :key="i">
+                <q-tab :name="'sample' + i.toString()" :label="'Sample ' + i.toString()"></q-tab>
+              </template>
+            </q-tabs>
+          </q-card-section>
+          <q-card-section>
+            <template v-for="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]" :key="i">
+              <q-btn :label="'Sample ' + i.toString()" @click="tab = 'sample' + i.toString()"></q-btn>
+            </template>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-12 col-md-4"></div>
+    </div>
+  </q-page>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const tab = ref('sample1')
+</script>
